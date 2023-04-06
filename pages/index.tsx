756d1adddc59c0d0755e0c4335d8cc7f4706d70e
김진영 <jinyoung01099@gmail.com>
2023-04-06 21:08:30 +0900

    [Add] getStaticProps 사용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-const Homepage = () => {
+interface HomepageProps {
+  products: { id: number; title: string }[];
+}
+
+const Homepage: React.FC<HomepageProps> = ({ products }) => {
   return (
     <ul>
-      <li>Product1</li>
-      <li>Product2</li>
-      <li>Product3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+};
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      products: [
+        { id: 1, title: "Product 1" },
+        { id: 2, title: "Product 2" },
+      ],
+    },
+  };
 };
 
 export default Homepage;
